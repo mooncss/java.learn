bd8a9d147f79414ca196cc4955f066ee08535233
du xinlong <357182695@qq.com>
2021-07-14 16:57:43 +0800

    测试 For 循环 测试 HashMap 测试 集合类型

--- src/main/java/com/example/demo/controller/Test.java
+++ src/main/java/com/example/demo/controller/Test.java
@@ -3,6 +3,9 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collection;
+import java.util.HashMap;
+
 /*
 @author By 林中小鸟
 @package  com.example.demo.controller
@@ -14,6 +17,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class Test {
     @GetMapping("/one")
     public String test() {
+        HashMap<String,String> map = new HashMap<>();
+        map.put("项目","学习");
+        map.put("作者","MoonCss");
+        map.put("昵称","我是大魔王");
+        map.put("日期","2021-10-21");
+
+        map.remove("昵称");
+        Collection values = map.values();
+        for (Object val:values){
+            System.out.println("val.toString() = " + val.toString());
+        };
+        System.out.println("--------------------------------");
+        System.out.println("map = " + map);
         return "1232131232";
     }
 }
